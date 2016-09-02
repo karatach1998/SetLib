019869ca6d913d4e0028c7b82cc58ef0dff7cfe2
karatach1998 <karatach1998@gmail.com>
2016-09-02 22:56:06 +0300

    Класс реализует работу комбинаторных объектов в виде статических методов.

---
+++ SetLib/CombObjects.cpp
@@ -0,0 +1,49 @@
+//
+// Created by Сергей on 01.09.2016.
+//
+
+#include <functional>
+#include "CombObjects.h"
+#include "SetOrderedArray.h"
+
+
+
+void CombObjects::bitVector( unsigned n, void (*f)( std::vector<bool>& ))
+{
+    std::vector<bool> vect(n, false);
+    std::function<void(unsigned)> genBitVector = [&]( unsigned i ) {
+        for (int x = 0; x <= 1; ++x) {
+            vect[i] = x == 1;
+            if (i == n - 1)
+                f( vect );
+            else
+                genBitVector(i + 1);
+        }
+    };
+
+
+    genBitVector(0);
+}
+
+
+
+template<typename T>
+void CombObjects::placing( SetBase<T>& set, unsigned k, void (* f)( std::vector<T> ))
+{
+    std::vector<T> v(k, 0);
+    unsigned n = (unsigned int) set.getPower();
+
+    std::function<void(SetBase<T>, unsigned)> genPlacing = [&]( SetBase<T> M, unsigned i ) {
+        for (T x : M) {
+            v[i] = x;
+            if (i == k - 1)
+                f(v);
+            else
+                genPlacing(M - {x}, i + 1);
+        }
+    };
+
+    genPlacing(set, 0);
+}
+
+
---
+++ SetLib/CombObjects.h
@@ -0,0 +1,27 @@
+//
+// Created by Сергей on 30.08.2016.
+//
+
+#ifndef CPP_SET_LIB_COMBOBJECTS_H
+#define CPP_SET_LIB_COMBOBJECTS_H
+
+
+#include <vector>
+#include "SetBase.h"
+
+
+class CombObjects
+{
+private:
+
+public:
+    typedef std::vector<bool>& BinaryVector;
+
+    static void bitVector( unsigned n, void (*f)( BinaryVector ));
+
+    template <typename T>
+    static void placing( SetBase<T>& set, unsigned k, void (* f)( std::vector<T> ));
+};
+
+
+#endif //CPP_SET_LIB_COMBOBJECTS_H
